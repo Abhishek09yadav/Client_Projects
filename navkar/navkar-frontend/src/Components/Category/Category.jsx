73098b335ce3278b0c53a992a0308c6d4c2b8da3
Abhishek09yadav <monuy8830@gmail.com>
2024-12-15 01:26:23 +0530

    Adds quotation history page and refactors product display.

--- navkar/navkar-frontend/src/Components/Category/Category.jsx
+++ navkar/navkar-frontend/src/Components/Category/Category.jsx
@@ -249,6 +249,7 @@ const Category = () => {
                                                 />
                                             </div>
                                         )}
+
                                     </div>
                                 ))}
                             </div>
