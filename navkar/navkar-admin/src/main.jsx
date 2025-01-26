803233f0123290a1c7954c13771b78acb38816fd
Abhishek09yadav <monuy8830@gmail.com>
2025-01-26 18:58:59 +0530

    configuring deployment

--- navkar/navkar-admin/src/main.jsx
+++ navkar/navkar-admin/src/main.jsx
@@ -1,16 +1,16 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ShopContextProvider from '../Context/ShopContext.jsx'
-import {BrowserRouter} from "react-router-dom";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import ShopContextProvider from '../Context/ShopContext.jsx';
+import {HashRouter} from 'react-router-dom'; // Import HashRouter instead of BrowserRouter
 
 createRoot(document.getElementById('root')).render(
-    <ShopContextProvider> <StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-
-    </StrictMode></ShopContextProvider>
-    ,
-)
+    <ShopContextProvider>
+        <StrictMode>
+            <HashRouter> {/* Replace BrowserRouter with HashRouter */}
+                <App/>
+            </HashRouter>
+        </StrictMode>
+    </ShopContextProvider>
+);
--- navkar/navkar-frontend/src/App.js
+++ navkar/navkar-frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom"; // Import HashRouter instead of BrowserRouter
 import Shop from "./Pages/Shop";
 // import 'bootstrap/dist/js/bootstrap.min.js';
 import Product from "./Pages/Product";
@@ -9,27 +9,22 @@ import Footer from "./Components/Footer/Footer";
 import LoginSignup from "./Pages/LoginSignup";
 import QuotationHistory from "./Components/QuotationHistory/QuotationHistory";
 
-
 function App() {
     return (
         <div>
             {/*<AlertProvider template={AlertTemplate} {...options}>*/}
-            <BrowserRouter>
+            <HashRouter> {/* Replace BrowserRouter with HashRouter */}
                 <Navbar/>
                 <Routes>
-
                     <Route path='/' element={<Shop/>}/>
                     <Route path='/product' element={<Product/>}>
                         <Route path=':productId' element={<Product/>}/>
                     </Route>
                     <Route path='/login' element={<LoginSignup/>}/>
                     <Route path='/quotationhistory' element={<QuotationHistory/>}/>
-
-
-
                 </Routes>
                 <Footer/>
-            </BrowserRouter>
+            </HashRouter> {/* Replace BrowserRouter with HashRouter */}
             {/*</AlertProvider>*/}
         </div>
     );
