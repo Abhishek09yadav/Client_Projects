79c89d1ff0387366e0a096a9ac419cdc327b224c
Abhishek09yadav <monuy8830@gmail.com>
2025-02-25 16:33:54 +0530

    Enhances form fields with icons and refactors styling.

--- plywoodwale/frontend/src/ContactForm.jsx
+++ plywoodwale/frontend/src/ContactForm.jsx
@@ -1,6 +1,13 @@
 import {useState} from "react";
 import PropTypes from "prop-types";
 import {TextField, MenuItem, Button, Grid2} from "@mui/material";
+import {FaUser} from "react-icons/fa";
+import {RiWhatsappFill} from "react-icons/ri";
+import {HiBuildingOffice} from "react-icons/hi2";
+import {HiMiniUserGroup} from "react-icons/hi2";
+import {FaLocationDot} from "react-icons/fa6";
+import {FaSquareShareNodes} from "react-icons/fa6";
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import style from "./first.module.css";
 import "./common.css";
@@ -64,20 +71,24 @@ const ContactForm = ({setFormNumber}) => {
             <Grid2 container spacing={2}>
                 <div
                     className={`text-dark w-100 d-flex gap-3 flex-md-row  flex-column justify-content-md-start justify-content-center align-content-md-start align-items-center`}>
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
                         borderRadius: "5px",
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
                     }}>
+                        <FaUser className={'m-2'}/>
                         <TextField fullWidth label="Your Name" name="name" value={formData.name} onChange={handleChange}
                                    required/>
                     </Grid2>
 
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
                         borderRadius: "5px",
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
                     }}>
+                        <RiWhatsappFill className={'m-2'}/>
                         <TextField
                             fullWidth
                             label="WhatsApp Number"
@@ -88,11 +99,13 @@ const ContactForm = ({setFormNumber}) => {
                             placeholder="10-digit number"
                             required
                         />
+
                     </Grid2>
                 </div>
                 <div
                     className={`text-dark w-100 d-flex gap-3 flex-md-row  flex-column justify-content-md-start justify-content-center align-content-md-start align-items-center`}>
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
                         background: "white",
                         color: "black",
                         width: "45%",
@@ -100,10 +113,12 @@ const ContactForm = ({setFormNumber}) => {
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
                     }}>
+                        <HiBuildingOffice className={'m-2'}/>
                         <TextField fullWidth label="Company" name="company" value={formData.company}
                                    onChange={handleChange}/>
                     </Grid2>
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
                         background: "white",
                         color: "black",
                         width: "45%",
@@ -111,6 +126,7 @@ const ContactForm = ({setFormNumber}) => {
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
                     }}>
+                        <HiMiniUserGroup className={'m-2'}/>
                         <TextField select fullWidth label="You are" name="youAre" value={formData.youAre}
                                    onChange={handleChange} required>
                             <MenuItem value="">Select Role</MenuItem>
@@ -122,7 +138,8 @@ const ContactForm = ({setFormNumber}) => {
                 </div>
                 <div
                     className={`text-dark w-100 d-flex gap-3 flex-md-row  flex-column justify-content-md-start justify-content-center align-content-md-start align-items-center`}>
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
                         background: "white",
                         color: "black",
                         width: "45%",
@@ -130,20 +147,21 @@ const ContactForm = ({setFormNumber}) => {
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
                     }}>
+                        <FaLocationDot className={'m-2'}/>
                         <TextField fullWidth multiline rows={3} label="Address" name="address" value={formData.address}
                                    onChange={handleChange} required/>
                     </Grid2>
 
 
-                    <Grid2 xs={12} className={`bg-white text-dark w-50`} id={"responsive"} sx={{
-                        background: "white",
-                        color: "black",
+                    <Grid2 xs={12} className={`bg-white text-dark w-50 d-flex flex-row align-items-center`}
+                           id={"responsive"} sx={{
+
                         width: "45%",
                         height: "fit-content",
                         borderRadius: "5px",
                         '& .MuiInputLabel-root': {color: 'navy'},
                         '& .MuiInputLabel-root.Mui-focused': {color: 'cyan'}
-                    }}>
+                    }}> <FaSquareShareNodes className={'m-2'}/>
                         <TextField fullWidth label="Reference" name="reference" value={formData.reference}
                                    onChange={handleChange}/>
                     </Grid2>
