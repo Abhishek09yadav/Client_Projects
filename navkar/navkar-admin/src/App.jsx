c63ebbcaa747ecc7909caa376a8c91ed0c400c05
Abhishek Yadav <monuy8830@gmail.com>
2025-03-12 09:37:47 +0530

    feat: Add loading spinner and server check in App component for improved user experience

--- navkar/navkar-admin/src/App.jsx
+++ navkar/navkar-admin/src/App.jsx
@@ -1,22 +1,36 @@
-import {useContext, useState} from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
 import Admin from "./Pages/Admin/Admin.jsx";
 // index.js or App.js
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {ShopContext} from "../Context/ShopContext.jsx"
+import "bootstrap/dist/css/bootstrap.min.css";
 import LoginSignup from "./Components/LoginSignup/LoginSignup.jsx";
-function App() {
-    const [count, setCount] = useState(0)
-    const { userDetails } = useContext(ShopContext);
-    const authToken = localStorage.getItem("auth-token");
-    return (
-        <>
-            <Navbar/>
+import Spinner from "./Components/spinner/Spinner.jsx";
 
-            {authToken ? <Admin/>: <LoginSignup/>}
-        </>
-    )
+function App() {
+  const [loading, setLoading] = useState(false);
+  const authToken = localStorage.getItem("auth-token");
+  useEffect(() => {
+    const checkServer = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`/`);
+        console.log("response", response);
+      } catch (error) {
+        console.error("Error fetching banners:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkServer();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      {loading? <Spinner /> :(authToken ? <Admin /> : <LoginSignup />)}
+    </>
+  );
 }
 
-export default App
+export default App;
